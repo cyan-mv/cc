1b1274b02648168a897ed819274cb23fc632a06e
cyan-mv <cyan.mv@gmail.com>
2024-10-06 16:42:53 -0600

    Role/ User relation manager

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -15,4 +15,7 @@ class Role extends Model
         return $this->belongsToMany(Permission::class);
     }
 
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
 }
